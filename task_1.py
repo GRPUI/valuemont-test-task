753a7203929c45bb1c24ed113ac31e9303dc5f2c
Yaroslav Galitsyn <galicin20045@gmail.com>
2024-03-22 13:19:41 +0700

    Refactor code in task_1

--- task_1.py
+++ task_1.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-score, attempts = pd.read_excel('test_data.xlsx', sheet_name=['Score', 'Attemps']).values()
-attempts = attempts.sort_values(by='Date', ascending=True).drop_duplicates(subset='Player', keep='last')
-score_attempts = pd.merge(score, attempts, on='Player', how='inner')
-score_attempts['Date'] = score_attempts['Date'].dt.strftime('%d.%m.%Y')
-score_attempts['User'] = ["yaroslav.galitsyn@valuemont.com" for i in range(len(score_attempts))]
 
-score_attempts.to_excel('result.xlsx', index=False)
+def main():
+    score, attempts = pd.read_excel('test_data.xlsx', sheet_name=['Score', 'Attemps']).values()
+    attempts = attempts.sort_values(by='Date', ascending=True).drop_duplicates(subset='Player', keep='last')
+    score_attempts = pd.merge(score, attempts, on='Player', how='inner')
+    score_attempts['Date'] = score_attempts['Date'].dt.strftime('%d.%m.%Y')
+    score_attempts['User'] = ["yaroslav.galitsyn@valuemont.com" for i in range(len(score_attempts))]
+
+    score_attempts.to_excel('result.xlsx', index=False)
+
+
+if __name__ == '__main__':
+    main()
